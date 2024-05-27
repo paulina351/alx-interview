3423823fb7ab0b264ae05c2adfcd8f40c6e65e1b
Paulina Udeh <udehpaulina93@gmail.com>
2024-05-27 01:42:59 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,28 +4,18 @@ Minimum Operations
 """
 
 
-def primeFactor(z):
-    """Getting the prime
-    """
-    divide = 2
-    array = list()
-    while (divide <= z):
-        if z % divide == 0:
-            array.append(divide)
-            z /= divide
-        else:
-            divide += 1
-
-    return array
-
-
 def minOperations(n):
     """A method that calculates the fewest number of operations
         needed to result in exactly n H characters in the file.
     """
-    minimun = 0
-    fact = [z for z in primeFactor(n)]
-    appearances = {item: fact.count(item) for item in fact}
-    for a, b in appearances.items():
-        minimum += a * b
-    return minimum
+    if n <= 1:
+        return 0
+    numb, div, numOfOprationss = n, 2, 0
+
+    while numb > 1:
+        if numb % div == 0:
+            numb = numb / div
+            numOfOprationss = numOfOprationss + div
+        else:
+            div += 1
+    return numOfOprationss
